e3adce70f5abe7f005da3a14fc17b513c154d9eb
ahmetkaanozbek <ahmetkaanozbek@hotmail.com>
2024-01-19 12:48:56 +0300

    Added updateCategory. However, it only updates the name of the category. I think there is no need to update category with its products.

--- src/main/java/com/aozbek/ecommerce/controller/CategoryController.java
+++ src/main/java/com/aozbek/ecommerce/controller/CategoryController.java
@@ -4,10 +4,7 @@ import com.aozbek.ecommerce.model.Category;
 import com.aozbek.ecommerce.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/category")
@@ -23,5 +20,12 @@ public class CategoryController {
     public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
         Category savedCategory = categoryService.saveCategory(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @PatchMapping(value = "/update/{categoryId}")
+    public ResponseEntity<Category> updateCategory(@RequestBody Category category,
+                                                   @PathVariable Long categoryId) {
+        Category updatedCategory = categoryService.updateCategory(category, categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedCategory);
     }
 }
--- src/main/java/com/aozbek/ecommerce/service/CategoryService.java
+++ src/main/java/com/aozbek/ecommerce/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.aozbek.ecommerce.service;
 
+import com.aozbek.ecommerce.exception.CategoryNotExist;
 import com.aozbek.ecommerce.model.Category;
 import com.aozbek.ecommerce.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,14 @@ public class CategoryService {
 
     public Category saveCategory(Category category) {
         return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category, Long categoryId) {
+        if (!(categoryRepository.existsById(categoryId))) {
+            throw new CategoryNotExist();
+        }
+        Category updatedCategory = categoryRepository.getReferenceById(categoryId);
+        updatedCategory.setCategoryName(category.getCategoryName());
+        return categoryRepository.save(updatedCategory);
     }
 }
