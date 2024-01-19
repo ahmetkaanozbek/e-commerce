cd842c171b9a444cc9b52e923fbb2058c0d326c5
ahmetkaanozbek <ahmetkaanozbek@hotmail.com>
2024-01-19 19:37:17 +0300

    Added a temporary User entity for make the CartService work. Also added CartItem to create a CartService.

---
+++ src/main/java/com/aozbek/ecommerce/model/CartItem.java
@@ -0,0 +1,28 @@
+package com.aozbek.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    User user;
+}
